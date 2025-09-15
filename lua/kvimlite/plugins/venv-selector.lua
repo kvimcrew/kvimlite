46100309546e7f9bfcd43126db4f04216420bc20
Kim Desrosiers <kimdesro@gmail.com>
2025-09-15 09:18:31 -0400

    Update to match new lazyvim changes

--- lua/kvimlite/plugins/venv-selector.lua
+++ lua/kvimlite/plugins/venv-selector.lua
@@ -1,6 +1,6 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+  dependencies = { "neovim/nvim-lspconfig", "mfussenegger/nvim-dap-python" },
   branch = "main",
   opts = {
     options = {
