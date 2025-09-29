fadd229e57fc2880019a0c12a734f3e2b536c48b
Kim Desrosiers <kimdesro@gmail.com>
2025-09-29 10:16:27 -0400

    Remove bufferline temp fix

--- lua/kvimlite/plugins/bufferline.lua
+++
@@ -1,9 +0,0 @@
--- Remove when https://github.com/LazyVim/LazyVim/pull/6354 will be merged
-return {
-  "catppuccin",
-  optional = true,
-  opts = function()
-    local bufferline = require("catppuccin.groups.integrations.bufferline")
-    bufferline.get = bufferline.get or bufferline.get_theme
-  end,
-}
